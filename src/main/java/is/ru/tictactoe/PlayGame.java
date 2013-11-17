6d4e05b94041c6594448d4c18eb3f5153cc45d1b
gudmundurhar12 <gudmundurhar12@ru.is>
2013-11-17 16:37:01 +0000

    Made input fields for names in index file. Also added function to construct Player instances in PlayGame.java

--- src/main/java/is/ru/tictactoe/PlayGame.java
+++ src/main/java/is/ru/tictactoe/PlayGame.java
@@ -10,24 +10,17 @@ public class PlayGame{
 
 // ## JUST SOME BASE CODE BASED ON HELLOWORLD.JAVA FROM HELLOWORLD DEMO ##
 
-//		staticFileLocation("/public");
-//		post(new Route("/add")
-//		{
-//			@Override
-//			public Object handle(Request request, Response response)
-//			{
-//				Player player0 = new Player(0, (request.queryParams("Player0")));
-//				Player player1 = new Player(1, (request.queryParams("Player1")));
-//				return 0;
-//			}
-//
-//		});
-		
+	
 	staticFileLocation("/public");
 	post(new Route("/add") {
              @Override
              public Object handle(Request request, Response response) {
-                 return "Hhhoooowwwdyhowww";
+                String name0 = (String.valueOf(request.queryParams("player0")));
+				String name1 = (String.valueOf(request.queryParams("player1")));
+				
+				Player player0 = new Player(0, name0);
+				Player player1 = new Player(1, name1);
+				return name0 + " " + name1;
              }
          });
 
