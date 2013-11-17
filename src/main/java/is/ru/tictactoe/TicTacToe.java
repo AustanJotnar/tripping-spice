201938ebacfe1b0ec1f83620456e748a1fcd98eb
Emil Atli <emilatli@gmail.com>
2013-11-17 11:18:15 +0000

    Name convention fyrir folder path lagað ásamt deploy í samræmi við það og svo package nöfnunum

---
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -0,0 +1,14 @@
+package is.ru.tictactoe;
+
+
+public class TicTacToe 
+{
+    public static void main(String[] args) 
+    {
+
+	System.out.println("TicTacToe");
+	//TODO this is commit;
+    
+    }
+}
+
---
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -0,0 +1,13 @@
+package is.ru.tictactoe;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TicTacToeTest 
+{
+    @Test
+    public void testTicTacToe() 
+    {
+        assertEquals("TicTacToe", "TicTacToe");
+    }
+}
+
