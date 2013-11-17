ec56f0058c1b18d5f15d731f169b6b2eb0e1c42f
gudmundurhar12 <gudmundurhar12@ru.is>
2013-11-17 22:22:44 +0000

    A possible fundation for the selenium testing was made, based on the course Demo

---
+++ src/test/java/is/ru/tictactoe/PageTestFireFox.java
@@ -0,0 +1,48 @@
+package is.ru.tictactoe;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class PageTestFireFox {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = System.getenv("STAGING_SERVER");
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void titleTest() throws Exception {
+    driver.get(baseUrl);
+    assertEquals("TicTacToe", driver.getTitle());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+}
--- src/test/java/is/ru/tictactoe/PlayGameTest.java
+++ src/test/java/is/ru/tictactoe/PlayGameTest.java
@@ -1,12 +1,25 @@
-package is.ru.tictactoe;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+// Old Selenium test code possibly wourthless
 
-public class PlayGameTest 
-{
-    @Test
-    public void testPlayGame() 
-    {
-        assertEquals("PlayGame", "PlayGame");
-    }
-}
+// package is.ru.tictactoe;
+
+// import junit.framework.TestCase;
+// import com.thoughtworks.selenium.DefaultSelenium;
+
+// public class PlayGameTest extends TestCase
+// {
+
+//     protected DefaultSelenium createSeleniumClient(String url) throws Exception
+//     {
+//     	return new DefaultSelenium("localhost",4444, "*firefox",url);
+//     }
+    
+//     public void testTitle() throws Exception 
+//     {
+//         DefaultSelenium selenium = createSeleniumClient("http://localhost:4567");
+//         selenium.start();
+//         selenium.open("/");
+//         selenium.waitForPageToLoad("3000");
+//         assertEquals("TicTacToe", selenium.getTitle());
+//         selenium.stop();
+//     }
+// }
