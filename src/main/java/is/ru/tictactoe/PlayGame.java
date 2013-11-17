cc6cd6c6cc5adbfd41a8a43d16076bee3548047a
gudmundurhar12 <gudmundurhar12@ru.is>
2013-11-17 13:40:11 +0000

    Some code based on the helloworld demo from the seminar - all commented out though

--- src/main/java/is/ru/tictactoe/PlayGame.java
+++ src/main/java/is/ru/tictactoe/PlayGame.java
@@ -1,9 +1,30 @@
 package is.ru.tictactoe;
+import static spark.Spark.*;
+import spark.*;
+
 
 public class PlayGame{
 
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
 
-		System.out.println("PlayGame");
+// ## JUST SOME BASE CODE BASED ON HELLOWORLD.JAVA FROM HELLOWORLD DEMO ##
+
+//		staticFileLocation("/public");
+//		post(new Route("/add")
+//		{
+//			@Override
+//			public Object handle(Request request, Response response)
+//			{
+//				Player player0 = new Player(0, (request.queryParams("Player0")));
+//				Player player1 = new Player(1, (request.queryParams("Player1")));
+//				return 0;
+//			}
+//
+//		});
+		
+		System.out.println("PlayGame"); 
+
+
 	}
 }
