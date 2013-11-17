adccb2af9dfcef4841c1c9dc2913469f1ccbe211
sveinn <sveinn@linuxmint.linuxmint-domain>
2013-11-17 14:53:44 +0000

    Added resetBoard() + testGameBoardtestGameBoardEmpty() and testGameBoardAddToBoardXon0()

--- src/main/java/is/ru/tictactoe/GameBoard.java
+++ src/main/java/is/ru/tictactoe/GameBoard.java
@@ -6,7 +6,28 @@ public class GameBoard
     private char[] board;
     public GameBoard()
     {
-    	board = new char[9];
+    	this.board = new char[9];
+    	this.resetBoard();
     }
     
+    public char[] getBoard()
+    {
+    	return this.board;
+    }
+    
+    public void addToBoard(char token, int pos)
+    {
+    	if( (token == 'X' || token == 'O') && this.board[pos] == 'f')
+    	{
+    		this.board[pos] = token;
+    	}
+    }
+    
+    public void resetBoard()
+    {
+    	for(int i = 0; i < 9; i++)
+    	{
+    		this.board[i] = 'f';
+    	}
+    }  
 }
--- src/test/java/is/ru/tictactoe/GameBoardTest.java
+++ src/test/java/is/ru/tictactoe/GameBoardTest.java
@@ -1,13 +1,33 @@
 package is.ru.tictactoe;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GameBoardTest 
 {
+    GameBoard gb = new GameBoard();    
     @Test
-    public void testGameBoard() 
+    public void testGameBoardEmpty() 
     {
-        assertEquals("GameBoard", "GameBoard");
+        char test[] = new char[9];
+        for(int i = 0; i < 9; i++)
+        {
+        	test[i] = 'f';
+        }
+        assertArrayEquals(gb.getBoard(), test);
     }
+
+    @Test
+    public void testGameBoardSize() 
+    {
+        assertEquals(gb.getBoard().length, 9);
+    }
+    
+    @Test
+    public void testGameBoardAddToBoardXon0() 
+    {
+        gb.addToBoard('X', 0);
+        assertEquals(gb.getBoard()[0], 'X');
+    }
+
 }
 
