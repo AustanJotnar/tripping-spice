90015082e70c17cad0b0760f0c5c623c82980fd2
Emil Atli <emilatli@gmail.com>
2013-11-19 13:42:11 +0000

    OMG

--- src/test/java/is/ru/tictactoe/FireFoxIT.java
+++ src/test/java/is/ru/tictactoe/FireFoxIT.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 import java.util.regex.Pattern;
 import static org.apache.commons.lang3.StringUtils.join;
 
-public class FireFoxIT {
+public class FireFoxTestTitleIT {
 	private Selenium selenium;
 
 	@Before
