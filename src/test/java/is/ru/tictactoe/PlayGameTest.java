3354259455518a1bb1e4752f1eef872cccee83b0
gudmundurhar12 <gudmundurhar12@ru.is>
2013-11-20 20:11:03 +0000

    Added testing for PlayGame.java

--- src/test/java/is/ru/tictactoe/PlayGameTest.java
+++ src/test/java/is/ru/tictactoe/PlayGameTest.java
@@ -1,25 +1,38 @@
-// Old Selenium test code possibly wourthless
+package is.ru.tictactoe;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-// package is.ru.tictactoe;
+public class PlayGameTest 
+{	
 
-// import junit.framework.TestCase;
-// import com.thoughtworks.selenium.DefaultSelenium;
-
-// public class PlayGameTest extends TestCase
-// {
-
-//     protected DefaultSelenium createSeleniumClient(String url) throws Exception
-//     {
-//     	return new DefaultSelenium("localhost",4444, "*firefox",url);
-//     }
-    
-//     public void testTitle() throws Exception 
-//     {
-//         DefaultSelenium selenium = createSeleniumClient("http://localhost:4567");
-//         selenium.start();
-//         selenium.open("/");
-//         selenium.waitForPageToLoad("3000");
-//         assertEquals("TicTacToe", selenium.getTitle());
-//         selenium.stop();
-//     }
-// }
+	Player p1 = new Player(0, "Alice");
+    Player p2 = new Player(1, "Bob");
+	TicTacToe ticgame = new TicTacToe(p1,p2);
+	PlayGame game = new PlayGame();
+	@Test
+    public void testPlayWin() 
+    {
+    	ticgame.gameBoard.addToBoard('X', 0);
+		ticgame.gameBoard.addToBoard('X', 1);
+    	assertEquals("F2XAlice won!", game.play(ticgame, 2));
+    }
+    @Test
+    public void testPlaySwitch()
+    {
+    	assertEquals("P0XBob's move", game.play(ticgame, 0));
+    }
+    @Test
+    public void testPlayDraw()
+    {
+    	ticgame.gameBoard.addToBoard('X', 0);
+		ticgame.gameBoard.addToBoard('O', 1);
+		ticgame.gameBoard.addToBoard('X', 2);
+		ticgame.gameBoard.addToBoard('O', 3);
+		ticgame.gameBoard.addToBoard('X', 4);
+		ticgame.gameBoard.addToBoard('O', 5);
+		ticgame.gameBoard.addToBoard('X', 7);
+		ticgame.gameBoard.addToBoard('O', 6);
+		ticgame.switchPlayer();
+		assertEquals("F8OIt's a draw!", game.play(ticgame, 8));
+    }
+}
