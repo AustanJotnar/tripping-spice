bff186776ad9f7469e0f7ebe7948383b5437bed9
Kjartan Svanur <kjartan.svanur.h@gmail.com>
2013-11-23 18:20:21 +0000

    Possibly avoided the Selenium test errors from testing DOM-element values

--- src/test/java/is/ru/tictactoe/FireFoxCheckIT.java
+++ src/test/java/is/ru/tictactoe/FireFoxCheckIT.java
@@ -9,15 +9,17 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
 import static org.apache.commons.lang3.StringUtils.join;
 
 public class FireFoxCheckIT {
 	private Selenium selenium;
+	private WebDriver driver;
 
 	@Before
 	public void setUp() throws Exception {
-		WebDriver driver = new FirefoxDriver();
 		String baseUrl = System.getenv("STAGING_SERVER");
+		driver = new FirefoxDriver();
 		selenium = new WebDriverBackedSelenium(driver, baseUrl);
 	}
 
@@ -39,7 +41,8 @@ public class FireFoxCheckIT {
 		selenium.click("id=cell1");
 		selenium.click("id=cell0");
 		selenium.click("id=cell4");
-		selenium.click("id=cell6");
+		selenium.click("id=cell6");		
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 		assertEquals("Simon won!", selenium.getText("id=messages"));
 	}
 
@@ -55,7 +58,8 @@ public class FireFoxCheckIT {
 		selenium.click("id=cell1");
 		selenium.click("id=cell3");
 		selenium.click("id=cell8");
-		selenium.click("id=cell6");
+		selenium.click("id=cell6");		
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 		assertEquals("Bob won!", selenium.getText("id=messages"));
 		selenium.click("css=button.btn.btn-default");
 		selenium.click("id=cell3");
@@ -67,6 +71,7 @@ public class FireFoxCheckIT {
 		selenium.click("id=cell7");
 		selenium.click("id=cell8");
 		selenium.click("id=cell2");
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 		assertEquals("It's a draw!", selenium.getText("id=messages"));
 	}
 
